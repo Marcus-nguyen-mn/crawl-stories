5dfc4ed487d8197769bcf9a4039088389c409bc5
Marcus <imkay2712@gmail.com>
2023-08-26 12:43:20 +0700

    edit

--- mc.js
+++ mc.js
@@ -1,26 +1,14 @@
-var request = require('request');
+var axios = require('axios');
 var cheerio = require('cheerio');
 var fs = require('fs');
+var mcArray = [];
 
-var download = function(uri, filename, callback){
-    request.head(uri, function(err, res, body){
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
+axios.get('https://mangakomi.io/manga/one-punch-man/chapter-1')
+.then(response => {
+    var $ = cheerio.load(response.data);
 
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-};
-
-request('https://mangaeffect.com/manga/one-punch-man/chapter-1', function(err, res, body){
-    if(err) console.log('Erro: '+ err);
-
-    var $ = cheerio.load(body);
-    var count = 0;
     $('.reading-content .page-break').each(function(){
-        count = count + 1;
         var mcGetImage = $(this).find('img').attr('data-src');
-        download(mcGetImage, `image/${count}.jpg`, function(){
-            console.log('done');
-        });
+        console.log('Image: ' + mcGetImage);
     })
-});
+})
