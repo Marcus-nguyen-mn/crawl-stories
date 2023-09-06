4b3e4ad528fcda0e2495285ab089d3ba4515afc4
Marcus <imkay2712@gmail.com>
2023-09-06 10:27:18 +0700

    version 3

--- mc.js
+++ mc.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var mcArray = [];
 var check = 0;
 var txtSave = '';
-var chap = 'chapter-2';
+var chap = 'chapter-1';
 
 async function downloadImage(url, filename) {
     const response = await axios.get(url, { responseType: 'arraybuffer' });
@@ -16,8 +16,7 @@ async function downloadImage(url, filename) {
     });
   }
   
-
-axios.get('https://mangaeffect.com/manga/spy-x-family/chapter-2')
+axios.get(`https://mangaeffect.com/manga/spy-x-family/${chap}`)
 .then(response => {
     var $ = cheerio.load(response.data);
     var count = 1;
@@ -33,7 +32,7 @@ axios.get('https://mangaeffect.com/manga/spy-x-family/chapter-2')
         downloadImage(element, `image/comics/spy-x-family/${chap}/${index+1}.jpg`);
         txtSave += `<img src="https://comicshito.com/wp-content/mcmedia/comics/spy-x-family/${chap}/${index+1}.jpg" class="content-comics" alt="Content Image">\n`;
     });
-    fs.writeFile(`image/comics/spy-x-family/${chap}/input.txt`, txtSave,  function(err) {
+    fs.writeFile(`image/comics/spy-x-family/${chap}.txt`, txtSave,  function(err) {
             if (err) {
                 return console.error(err);
             }
